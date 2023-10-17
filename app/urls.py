c5b294d6aec6db6fdc984f16ac9f6534a39f199f
gaith10 <gaithbenali184@gmail.com>
2023-10-17 18:45:36 +0100

    Registration & profile update

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView  # تأكد من استيراد UserLoginView من ملف العروض الصحيح
 from app.forms import UserLoginForm
+from app.views import RegisterView,edit_profile
 urlpatterns = [
     path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', edit_profile, name='profile'),
     path('', include('django.contrib.auth.urls'))
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import UserRegisterForm, ProfileForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    #success_url = reverse_lazy('login')
+    template_name = 'registration/register.html' #<----في هذا السطر
+    """def get_succes_url(self):
+        login(self.request, self.object)
+        return reverse_lazy('Project_list')"""
+    def get_success_url(self):
+       login(self.request, self.object)
+       return reverse_lazy('Project_List')
 
-# Create your views here.
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    form = ProfileForm(instance=request.user)
+    return render(request, 'profile.html',{
+        'form': form
+    })
